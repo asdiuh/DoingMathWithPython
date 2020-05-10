8810075a9c2f4b44a934e706ed562c0bc77462a2
redhwanzaman1989 <redhwanzaman1989@gmail.com>
2020-05-11 00:06:50 +0100

    got the DoingMathWithPython book projectile motion code working - one of the return statements werent indented properly

--- Code6.py
+++ Code6.py
@@ -295,11 +295,8 @@ from matplotlib import pyplot as plt
 from matplotlib import animation
 import math
 import numpy as np
-
 g = 9.8
-
 def update_position(i, circle, intervals, u, theta):
-
     t = intervals[i]
     t = float(t)
     x = u*math.cos(theta)*t
@@ -307,7 +304,6 @@ def update_position(i, circle, intervals, u, theta):
 #    circle.center = i, i
     circle.center = x, y
     return circle,
-
 u = 30
 theta = 45
 t_flight = 2*u*math.sin(theta)/g
@@ -317,23 +313,77 @@ interval = 0.005
 while start < t_flight:
     intervals.append(float(start))
     start = start + interval
-
 xmin = 0
 xmax = u*math.cos(theta)*intervals[-1]
 ymin = 0
-t_max = u*math.sin(theta)/gt
-ymax = u*math.sin(theta)*t_max - 0.5*g*t_max**2
+t_max = u*math.sin(theta)/g
+ymax = u*math.sin(theta)*t_max - 0.5*g*t_max**2 + 10
 fig = plt.gcf()
 ax = plt.axes(xlim=(xmin, xmax), ylim=(ymin, ymax))
-
+ax.set_aspect('equal')
 circle = plt.Circle((xmin, ymin), 1.0)
 ax.add_patch(circle)
 anim = animation.FuncAnimation(fig, update_position,
                     fargs=(circle, intervals, u, theta),
-                    frames=len(intervals), interval=1,
-                    repeat=False)
-
+                    frames=len(intervals), interval=.000001,
+                    repeat=True)
+#len(intervals)
 plt.title('Projectile Motion')
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.show()
+
+#**************************************************
+
+'''
+Animate the trajectory of an object in projectile motion
+'''
+
+from matplotlib import pyplot as plt
+from matplotlib import animation
+import math
+g = 9.8
+def get_intervals(u, theta):
+    t_flight = 2*u*math.sin(theta)/g
+    intervals = []
+    start = 0
+    interval = 0.005
+    while start < t_flight:
+        intervals.append(float(start))
+        start = start + interval
+    return intervals
+def update_position(i, circle, intervals, u, theta):
+    t = intervals[i]
+    t = float(t)
+    x = u*math.cos(theta)*t
+    y = u*math.sin(theta)*t - 0.5*g*t*t
+    circle.center = x, y
+#    circle.radius = i*0.5
+    return circle,
+def create_animation(u, theta):
+    intervals = get_intervals(u, theta)
+    xmin = 0
+    xmax = u*math.cos(theta)*intervals[-1]
+    ymin = 0
+    t_max = u*math.sin(theta)/g
+    ymax = u*math.sin(theta)*t_max - 0.5*g*t_max**2
+    fig = plt.gcf()
+    ax = plt.axes(xlim=(xmin, xmax), ylim=(ymin, ymax))
+    ax.set_aspect('equal')
+    circle = plt.Circle((xmin, ymin), 1.0)
+    ax.add_patch(circle)
+    anim = animation.FuncAnimation(fig, update_position,
+                                   fargs=(circle, intervals, u, theta),
+                                   frames=len(intervals), interval=1,
+                                   repeat=False)
+    plt.title('Projectile Motion')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.show()
+
+u = 45
+theta = 45
+theta = math.radians(theta)
+create_animation(u, theta)
+
+
