e295d9a249aef948af144f6ebf77b4396862dbb1
redhwanzaman1989 <redhwanzaman1989@gmail.com>
2020-05-10 13:51:01 +0100

    still trying to get the projectile motion bit to work - can get the circle moving, and can get the radius moving, but cant pass the projectile motion component to pass as an argument

--- Code6.py
+++ Code6.py
@@ -81,45 +81,40 @@ Animate the trajectory of an object in projectile motion
 from matplotlib import pyplot as plt
 from matplotlib import animation
 import math
-g = 9.8
 
-def get_intervals(u, theta):
-    t_flight = 2*u*math.sin(theta)/g
-    intervals = []
-    start = 0
-    interval = 0.005
-    while start < t_flight:
-        intervals.append(start)
-        start = start + interval
-    return intervals
-
-def update_position(i, circle, intervals, u, theta):
+def update_position(i):
     t = intervals[i]
     x = u*math.cos(theta)*t
     y = u*math.sin(theta)*t - 0.5*g*t*t
-    circle.center = x, y
+    circle.center = i, i
     return circle,
 
-def create_animation(u, theta):
-    theta = math.radians(theta)
-    intervals = get_intervals(u, theta)
-    xmin = 0
-    xmax = u*math.cos(theta)*intervals[-1]
-    ymin = 0
-    t_max = u*math.sin(theta)/g
-    ymax = u*math.sin(theta)*t_max - 0.5*g*t_max**2
-    fig = plt.gcf()
-    ax = plt.axes(xlim=(xmin, xmax), ylim=(ymin, ymax))
-    circle = plt.Circle((xmin, ymin), 1.0)
-    ax.add_patch(circle)
-    anim = animation.FuncAnimation(fig, update_position,
-                                   fargs=(circle, intervals, u, theta),
-                                   frames=len(intervals), interval=1,
-                                   repeat=False)
-    plt.title('Projectile Motion')
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.show()
+theta = 45
+u = 10
+g = 9.8
+theta = math.radians(theta)
+t_flight = 2*u*math.sin(theta)/g
+intervals = []
+start = 0
+interval = 0.005
+while start < t_flight:
+    intervals.append(start)
+    start = start + interval
+xmax = u*math.cos(theta)*intervals[-1]
+t_max = u*math.sin(theta)/g
+ymax = u*math.sin(theta)*t_max - 0.5*g*t_max**2
+fig = plt.gcf()
+circle = plt.Circle((0,0), 1.0)
+ax = plt.axes(xlim=(0, xmax), ylim=(0, ymax))
+ax.add_patch(circle)
+anim = animation.FuncAnimation(fig, update_position,
+                               fargs=(circle,),
+                               frames=len(intervals), interval=1,
+                               repeat=False)
+plt.title('Projectile Motion')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.show()
 
 create_animation(45, 15)
 
@@ -156,12 +151,73 @@ def throw(u,theta):
 throw(15,60)
 
 
+import matplotlib.pyplot as plt
+import animation as animation
 
+#***************************************************************
 
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
+fig, ax = plt.subplots()
 
+g = 9.8                                                        #value of gravity
+v = 10.0                                                       #initial velocity
+theta = 40.0 * np.pi / 180.0                                   #initial angle of launch in radians
+t = 2 * v * np.sin(theta) / g                                  
+t = np.arange(0, 0.1, 0.01)                                    #time of flight into an array
+x = np.arange(0, 0.1, 0.01)
+line, = ax.plot(x, v * np.sin(theta) * x - (0.5) * g * x**2)   # plot of x and y in time
 
+def animate(i):
+    """change the divisor of i to get a faster (but less precise) animation """
+    line.set_xdata(v * np.cos(theta) * (t + i /100.0))
+    line.set_ydata(v * np.sin(theta) * (x + i /100.0) - (0.5) * g * (x + i / 100.0)**2)  
+    return line,
 
+plt.axis([0.0, 10.0, 0.0, 5.0])
+ax.set_autoscale_on(False)
+
+ani = animation.FuncAnimation(fig, animate, np.arange(1, 200))
+plt.show()
+
+#**********************************************************
+
+import numpy as np
+import math
+from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+def update_radius(i, circle):
+    circle.radius = i
+    circle.center = i,i
+    return circle,
+
+u = 45
+theta = 45
+theta = math.radians(theta)
+g = 9.8
+ux = u*math.sin(theta)
+uy = u*math.cos(theta)
+total_time = 2*uy/g
+vx = ux
+vy = uy - g*t
+
+fig=plt.gcf()
+ax=plt.axes(xlim=(-10,10),ylim=(-10,10))
+ax.set_aspect('equal')
+#circle=create_circle()
+circle = plt.Circle((0, 0), 0.05)
+ax.add_patch(circle)
+anim=FuncAnimation(fig,
+                   update_radius,
+                   fargs=(circle,),
+                   frames=200,
+                   interval=1)
+plt.show()
+
+#*****************************************************
 
 
 
