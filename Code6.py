8681299354f2b73cfcbc0acf52e41536ed048795
redhwanzaman1989 <redhwanzaman1989@gmail.com>
2020-05-10 15:28:14 +0100

    got projectile motion working! - the change was that i changed the append to float - it was originally a list of strings, instead of numbers meaning that the center argument of the circle function was failing and nothing was being traced!

--- Code6.py
+++ Code6.py
@@ -83,9 +83,9 @@ from matplotlib import animation
 import math
 
 def update_position(i):
-    t = intervals[i]
-    x = u*math.cos(theta)*t
-    y = u*math.sin(theta)*t - 0.5*g*t*t
+#    t = intervals[i]
+#    x = u*math.cos(theta)*t
+#    y = u*math.sin(theta)*t - 0.5*g*t*t
     circle.center = i, i
     return circle,
 
@@ -109,7 +109,7 @@ ax = plt.axes(xlim=(0, xmax), ylim=(0, ymax))
 ax.add_patch(circle)
 anim = animation.FuncAnimation(fig, update_position,
                                fargs=(circle,),
-                               frames=len(intervals), interval=1,
+                               frames=100, interval=1,
                                repeat=False)
 plt.title('Projectile Motion')
 plt.xlabel('X')
@@ -157,18 +157,20 @@ import animation as animation
 #***************************************************************
 
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 fig, ax = plt.subplots()
 
-g = 9.8                                                        #value of gravity
-v = 10.0                                                       #initial velocity
-theta = 40.0 * np.pi / 180.0                                   #initial angle of launch in radians
-t = 2 * v * np.sin(theta) / g                                  
-t = np.arange(0, 0.1, 0.01)                                    #time of flight into an array
+g = 9.8 #value of gravity
+v = 10.0 #initial velocity
+theta = 40
+theta = 40.0 * np.pi / 180.0 #initial angle of launch in radians
+t = 2 * v * np.sin(theta) / g
+t = np.arange(0, 0.1, 0.01) #time of flight into an array
 x = np.arange(0, 0.1, 0.01)
-line, = ax.plot(x, v * np.sin(theta) * x - (0.5) * g * x**2)   # plot of x and y in time
+line, = ax.plot(x, v * np.sin(theta) * x - (0.5) * g * x**2) # plot of x and y in time
 
 def animate(i):
     """change the divisor of i to get a faster (but less precise) animation """
@@ -219,13 +221,119 @@ plt.show()
 
 #*****************************************************
 
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+fig, ax = plt.subplots()
+
+g = 9.8 #value of gravity
+v = 10 #initial velocity
+theta = 15
+theta = math.radians(theta) #initial angle of launch in radians
+t = 2 * u * math.sin(theta) / g
+t = np.arange(0, 0.1, 0.01) #time of flight into an array
+x = np.arange(0, 0.1, 0.01)
+line, = ax.plot(x, v * np.sin(theta) * x - (0.5) * g * x**2) # plot of x and y in time
+
+def animate(i):
+    """change the divisor of i to get a faster (but less precise) animation """
+    line.set_xdata(v * np.cos(theta) * (t + i /100.0))
+    line.set_ydata(v * np.sin(theta) * (x + i /100.0) - (0.5) * g * (x + i / 100.0)**2)  
+    return line,
+
+plt.axis([0.0, 10.0, 0.0, 5.0])
+ax.set_autoscale_on(False)
+
+ani = animation.FuncAnimation(fig, animate, np.arange(1, 200))
+plt.show()
+
+#*****************************************************
+
+import numpy as np
+import math
+from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+def update_radius(i, circle):
+    circle.radius = 10
+#    x = 
+    circle.center = i,i
+    return circle,
+
+u = 40
+theta = 30
+g = 9.8
+theta = math.radians(theta)
+ux = u*math.cos(theta)
+uy = u*math.sin(theta)
+t = 2 * uy / g
+interval = t/100
+t1 = []
+num = 0
+while num <= t:
+    t1.append(num)
+    num = num + interval
+
+fig=plt.gcf()
+ax=plt.axes(xlim=(-200,200),ylim=(-200,200))
+ax.set_aspect('equal')
+#circle=create_circle()
+circle = plt.Circle((0, 0), 10)
+ax.add_patch(circle)
+anim=FuncAnimation(fig,
+                   update_radius,
+                   fargs=(circle,),
+                   frames=200,
+                   interval=1)
+plt.show()
 
 
+#*************************************************************
+from matplotlib import pyplot as plt
+from matplotlib import animation
+import math
+import numpy as np
 
+g = 9.8
 
+def update_position(i, circle, intervals, u, theta):
 
+    t = intervals[i]
+    t = float(t)
+    x = u*math.cos(theta)*t
+    y = u*math.sin(theta)*t - 0.5*g*t*t
+#    circle.center = i, i
+    circle.center = x, y
+    return circle,
 
+u = 30
+theta = 45
+t_flight = 2*u*math.sin(theta)/g
+intervals = []
+start = 0
+interval = 0.005
+while start < t_flight:
+    intervals.append(float(start))
+    start = start + interval
 
+xmin = 0
+xmax = u*math.cos(theta)*intervals[-1]
+ymin = 0
+t_max = u*math.sin(theta)/g
+ymax = u*math.sin(theta)*t_max - 0.5*g*t_max**2
+fig = plt.gcf()
+ax = plt.axes(xlim=(xmin, xmax), ylim=(ymin, ymax))
 
+circle = plt.Circle((xmin, ymin), 1.0)
+ax.add_patch(circle)
+anim = animation.FuncAnimation(fig, update_position,
+                    fargs=(circle, intervals, u, theta),
+                    frames=len(intervals), interval=1,
+                    repeat=False)
 
-
+plt.title('Projectile Motion')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.show()
